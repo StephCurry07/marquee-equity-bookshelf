e024c689dd2a0ae889866d80a1f327b1a7a6ede7
Apoorv Gupta <apoorv.gupta65@gmail.com>
2024-06-11 12:39:36 +0530

    added loading

--- src/components/bookLibrary.jsx
+++ src/components/bookLibrary.jsx
@@ -1,66 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Books from '../api/books';
 import BookList from './BookList';
-import { debounce } from '../utils/debounce';
 
 const BookLibrary = () => {
   const [books, setBooks] = useState([]);
-  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleInput = (e) => {
+  const handleInput = async (e) => {
     const query = e.target.value;
-    setQuery(query);
-  }
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      if (!query) return;
-      try {
-        const res = await Books.get('/search.json', {
-          params: {
-            q: query,
-            limit: 10,
-            page: 1,
-          },
-        });
-        setBooks(res.data.docs);
-      } catch (error) {
-        alert(error.message);
-      }
-    };
-
-    const debouncedFetchBooks = debounce(fetchBooks, 500);
-    debouncedFetchBooks();
-
-    return () => {
-      debouncedFetchBooks.cancel();
-    };
-  }, [query]);
-
-  const addToBookShelf = (book) => {
-    let bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
-    bookshelf.push(book);
-    localStorage.setItem('bookshelf', JSON.stringify(bookshelf));
-    alert(`${book.title} added to bookshelf!`);
+    if (!query) return;
+    setLoading(true);
+    try {
+      const res = await Books.get('/search.json', {
+        params: {
+          q: query,
+          limit: 10,
+          page: 1,
+        },
+      });
+      setBooks(res.data.docs);
+      console.log(books);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return (
-    <div className="book-library">
-      <Header />
-      <p className="search-label">
-        Search by Book Name:
-      </p>
-      <input
-        type="text"
-        id="bookName"
-        name="bookName"
-        onChange={handleInput}
-        className="search-input"
-      />
+const addToBookShelf = (book) => {
+  let bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
+  bookshelf.push(book);
+  localStorage.setItem('bookshelf', JSON.stringify(bookshelf));
+  alert(`${book.title} added to bookshelf!`);
+};
+
+return (
+  <div className="book-library">
+    <Header />
+    <p className="search-label">
+      Search by Book Name:
+    </p>
+    <input
+      type="text"
+      id="bookName"
+      name="bookName"
+      onChange={handleInput}
+      className="search-input"
+    />
+    {loading ? (
+      <div className="spinner"></div>
+    ) : (
       <BookList books={books} onAddToBookShelf={addToBookShelf} />
-    </div>
-  );
+    )}
+  </div>
+);
 };
 
 export default BookLibrary;
